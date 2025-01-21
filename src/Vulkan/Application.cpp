9332bd3def94324880775b36e15afd13b0d5b97d
Tyler Smith <tylertms@icloud.com>
2025-01-20 19:51:42 -0500

    Add Device class

--- src/Vulkan/Application.cpp
+++ src/Vulkan/Application.cpp
@@ -3,6 +3,12 @@
 
 namespace VKAPP {
 
+Application::Application() {
+    m_AppWindow.init();
+    m_Instance.init();
+    m_Device.init(m_Instance.getVkInstance());
+}
+
 Application::~Application() {
 }
 
--- src/Vulkan/Application.h
+++ src/Vulkan/Application.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Device.h"
 #include "Instance.h"
 #include "Window.h"
 
@@ -7,12 +8,15 @@ namespace VKAPP {
 
 class Application {
   public:
+    Application();
     ~Application();
+    
     void run();
 
   private:
     Window m_AppWindow;
     Instance m_Instance;
+    Device m_Device;
 };
 
 } // namespace VKAPP
