2bc2d6f6c40687f0a8702312a73f4645c786915d
Tyler Smith <tylertms@icloud.com>
2025-01-22 15:31:43 -0500

    DescriptorSet class

---
+++ src/Vulkan/DescriptorSet.cpp
@@ -0,0 +1,57 @@
+#include "DescriptorSet.h"
+
+#include <stdexcept>
+
+namespace Vulkan {
+
+void DescriptorSet::deinit(const VkDevice &device) {
+    vkDestroyDescriptorSetLayout(device, m_DescriptorSetLayout, nullptr);
+}
+
+void DescriptorSet::createSet(const VkDevice &device, const Uniform &uniform, const VkDescriptorPool &descriptorPool) {
+    VkDescriptorSetAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool = descriptorPool;
+    allocInfo.descriptorSetCount = 1;
+    allocInfo.pSetLayouts = &m_DescriptorSetLayout;
+
+    if (vkAllocateDescriptorSets(device, &allocInfo, &m_DescriptorSet) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate descriptor sets!");
+    }
+
+    VkDescriptorBufferInfo bufferInfo{};
+    bufferInfo.buffer = uniform.getVkBuffer();
+    bufferInfo.offset = 0;
+    bufferInfo.range = sizeof(Uniform::Object);
+
+    VkWriteDescriptorSet descriptorWrite{};
+    descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    descriptorWrite.dstSet = m_DescriptorSet;
+    descriptorWrite.dstBinding = 0;
+    descriptorWrite.dstArrayElement = 0;
+    descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    descriptorWrite.descriptorCount = 1;
+    descriptorWrite.pBufferInfo = &bufferInfo;
+
+    vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
+}
+
+void DescriptorSet::createLayout(const VkDevice &device) {
+    VkDescriptorSetLayoutBinding uboLayoutBinding{};
+    uboLayoutBinding.binding = 0;
+    uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    uboLayoutBinding.descriptorCount = 1;
+    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+    uboLayoutBinding.pImmutableSamplers = nullptr;
+
+    VkDescriptorSetLayoutCreateInfo layoutInfo{};
+    layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    layoutInfo.bindingCount = 1;
+    layoutInfo.pBindings = &uboLayoutBinding;
+
+    if (vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &m_DescriptorSetLayout) != VK_SUCCESS) {
+        throw std::runtime_error("ERROR: Failed to create descriptor set layout.");
+    }
+}
+
+} // namespace Vulkan
---
+++ src/Vulkan/DescriptorSet.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Uniform.h"
+#include "VulkanApp.h"
+
+namespace Vulkan {
+
+class DescriptorSet {
+  public:
+    NO_COPY(DescriptorSet);
+    DescriptorSet() = default;
+
+    void createLayout(const VkDevice &device);
+    void createSet(const VkDevice &device, const Uniform &uniform, const VkDescriptorPool &descriptorPool);
+
+    void deinit(const VkDevice &device);
+
+    inline const VkDescriptorSetLayout &getVkDescriptorSetLayout() const { return m_DescriptorSetLayout; }
+    inline const VkDescriptorSet &getVkDescriptorSet() const { return m_DescriptorSet; }
+    
+  private:
+    VkDescriptorSet m_DescriptorSet;
+    VkDescriptorSetLayout m_DescriptorSetLayout;
+};
+
+} // namespace Vulkan
