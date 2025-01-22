b3a7da4badb99f40a40632cd7fe5742b152a42f9
Tyler Smith <tylertms@icloud.com>
2025-01-21 19:57:55 -0500

    fix dynamic state

--- src/Vulkan/GraphicsPipeline.cpp
+++ src/Vulkan/GraphicsPipeline.cpp
@@ -118,9 +118,7 @@ VkPipelineViewportStateCreateInfo GraphicsPipeline::getViewportState() const {
     VkPipelineViewportStateCreateInfo viewportState{};
     viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
     viewportState.viewportCount = 1;
-    viewportState.pViewports = &viewport;
     viewportState.scissorCount = 1;
-    viewportState.pScissors = &scissor;
 
     return viewportState;
 }
