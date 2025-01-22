558724169eceafdaa08232ba0e93b1aa5a550a9d
Tyler Smith <tylertms@icloud.com>
2025-01-22 17:40:03 -0500

    Allow imageview class to hold its own image (for non-swapchain imageviews)

--- src/Vulkan/ImageView.cpp
+++ src/Vulkan/ImageView.cpp
@@ -1,5 +1,8 @@
 #include "ImageView.h"
 
+#include "CommandPool.h"
+#include "Device.h"
+
 #include <stdexcept>
 
 namespace Vulkan {
@@ -7,9 +10,9 @@ namespace Vulkan {
 void ImageView::init(const VkDevice &device, const VkImage &image, const VkFormat &format) {
     VkImageViewCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-    createInfo.image = image;
+    createInfo.image = image == nullptr ? m_Image : image;
     createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-    createInfo.format = format;
+    createInfo.format = format == VK_FORMAT_UNDEFINED ? m_Format : format;
     createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
     createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
     createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
@@ -27,6 +30,97 @@ void ImageView::init(const VkDevice &device, const VkImage &image, const VkForma
 
 void ImageView::deinit(const VkDevice &device) {
     vkDestroyImageView(device, m_ImageView, nullptr);
+    vkDestroyImage(device, m_Image, nullptr);
+    vkFreeMemory(device, m_ImageMemory, nullptr);
+}
+
+void ImageView::createImage(const Device &device, VkExtent2D extent, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties) {
+    m_Format = format;
+
+    VkImageCreateInfo imageInfo{};
+    imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageInfo.imageType = VK_IMAGE_TYPE_2D;
+    imageInfo.extent.width = extent.width;
+    imageInfo.extent.height = extent.height;
+    imageInfo.extent.depth = 1;
+    imageInfo.mipLevels = 1;
+    imageInfo.arrayLayers = 1;
+    imageInfo.format = format;
+    imageInfo.tiling = tiling;
+    imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    imageInfo.usage = usage;
+    imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if (vkCreateImage(device.getVkDevice(), &imageInfo, nullptr, &m_Image) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create image!");
+    }
+
+    VkMemoryRequirements memRequirements;
+    vkGetImageMemoryRequirements(device.getVkDevice(), m_Image, &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = findMemoryType(device.getVkPhysicalDevice(), memRequirements.memoryTypeBits, properties);
+
+    if (vkAllocateMemory(device.getVkDevice(), &allocInfo, nullptr, &m_ImageMemory) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate image memory!");
+    }
+
+    vkBindImageMemory(device.getVkDevice(), m_Image, m_ImageMemory, 0);
+}
+
+void ImageView::transitionImageLayout(const Device &device, const CommandPool &commandPool, VkImageLayout oldLayout, VkImageLayout newLayout) {
+    VkCommandBuffer commandBuffer = commandPool.beginSingleTimeCommands(device.getVkDevice());
+
+    VkImageMemoryBarrier barrier{};
+    barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    barrier.oldLayout = oldLayout;
+    barrier.newLayout = newLayout;
+    barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.image = m_Image;
+    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    barrier.subresourceRange.baseMipLevel = 0;
+    barrier.subresourceRange.levelCount = 1;
+    barrier.subresourceRange.baseArrayLayer = 0;
+    barrier.subresourceRange.layerCount = 1;
+
+    VkPipelineStageFlags sourceStage;
+    VkPipelineStageFlags destinationStage;
+
+    if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+        barrier.srcAccessMask = 0;
+        barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+        sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        destinationStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+    } else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+        sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    } else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_GENERAL) {
+        barrier.srcAccessMask = 0;
+        barrier.dstAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+
+        sourceStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+        destinationStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+    } else {
+        throw std::invalid_argument("ERROR: Unsupported layout transition attempted.");
+    }
+
+    vkCmdPipelineBarrier(
+        commandBuffer,
+        sourceStage, destinationStage,
+        0,
+        0, nullptr,
+        0, nullptr,
+        1, &barrier);
+
+    commandPool.endSingleTimeCommands(commandBuffer, device);
 }
 
 } // namespace Vulkan
--- src/Vulkan/ImageView.h
+++ src/Vulkan/ImageView.h
@@ -1,21 +1,32 @@
 #pragma once
 
+#include "Buffer.h"
 #include "VulkanApp.h"
 
 namespace Vulkan {
+
+class CommandPool;
 
 class ImageView {
   public:
     NO_COPY(ImageView);
     ImageView() = default;
-    
+
     void init(const VkDevice &device, const VkImage &image, const VkFormat &format);
     void deinit(const VkDevice &device);
 
+    void createImage(const Device &device, VkExtent2D extent, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties);
+    void transitionImageLayout(const Device &device, const CommandPool &commandPool, VkImageLayout oldLayout, VkImageLayout newLayout);
+
+    inline const VkImage &getVkImage() const { return m_Image; }
     inline const VkImageView &getVkImageView() const { return m_ImageView; }
 
   private:
-    VkImageView m_ImageView;
+    VkImageView m_ImageView = nullptr;
+    VkImage m_Image = nullptr;
+    VkDeviceMemory m_ImageMemory = nullptr;
+
+    VkFormat m_Format;
 };
 
 } // namespace Vulkan
