f799bc0d0b1f559a510fb723954039c4d1640303
Tyler Smith <tylertms@icloud.com>
2025-01-22 17:45:19 -0500

    Remove unnecessary function

--- src/Vulkan/Buffer.h
+++ src/Vulkan/Buffer.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "Device.h"
 #include "VulkanApp.h"
 
 namespace Vulkan {
+
+class Device;
 
 uint32_t findMemoryType(const VkPhysicalDevice &physicalDevice, uint32_t typeFilter, VkMemoryPropertyFlags properties);
 void createBuffer(const Device &device, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer &buffer, VkDeviceMemory &bufferMemory);
