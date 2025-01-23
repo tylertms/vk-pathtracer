f73a118be6d21f8b7501f69bfa146e8a4044baa3
Tyler Smith <tylertms@icloud.com>
2025-01-23 15:14:14 -0500

    Add window size setter to window class

--- src/Vulkan/Window.h
+++ src/Vulkan/Window.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "VulkanApp.h"
+#include <cstdint>
 
 namespace Vulkan {
 
@@ -16,6 +17,11 @@ class Window {
     inline const uint32_t getWidth() const { return m_Width; }
     inline const uint32_t getHeight() const { return m_Height; }
     inline const float getAspectRatio() const { return (float)m_Width / m_Height; }
+
+    inline void setWindowSize(uint32_t width, uint32_t height) {
+        m_Width = width;
+        m_Height = height;
+    }
 
     static void onResize(GLFWwindow *window, int width, int height);
     bool wasResized();
