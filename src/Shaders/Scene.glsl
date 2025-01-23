0c9bebd87c467406cc74da32d04a14f1f761af70
Tyler Smith <tylertms@icloud.com>
2025-01-23 09:37:25 -0500

    Missing semicolons

--- src/Shaders/Scene.glsl
+++ src/Shaders/Scene.glsl
@@ -10,13 +10,13 @@ struct Material {
     vec3 color;
     float emissionStrength;
     vec3 emissionColor;
-}
+};
 
 struct Sphere {
     vec3 center;
     float radius;
     Material material;
-}
+};
 
 struct Scene {
     uint framesRendered;
