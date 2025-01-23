c157558a27654c43ec6ea417015e6b6d3a9cda6a
Tyler Smith <tylertms@icloud.com>
2025-01-23 11:09:14 -0500

    Use UniformObject struct

--- src/Vulkan/DescriptorSet.cpp
+++ src/Vulkan/DescriptorSet.cpp
@@ -22,7 +22,7 @@ void DescriptorSet::createSet(const VkDevice &device, const Uniform &uniform, co
     VkDescriptorBufferInfo bufferInfo{};
     bufferInfo.buffer = uniform.getVkBuffer();
     bufferInfo.offset = 0;
-    bufferInfo.range = sizeof(Uniform::Object);
+    bufferInfo.range = sizeof(UniformObject);
 
     VkDescriptorImageInfo imageInfo{};
     imageInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
