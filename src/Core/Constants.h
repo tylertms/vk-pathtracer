7cf3fe889565504ac71dfde1e0627be7a6364357
Tyler Smith <tylertms@icloud.com>
2025-01-26 16:50:57 -0500

    Add core unified constants

---
+++ src/Core/Constants.h
@@ -0,0 +1,13 @@
+#ifndef CORE_CONSTANTS_H
+#define CORE_CONSTANTS_H
+
+#define SAMPLES_PER_PIXEL 1
+#define MAX_BOUNCES 8
+
+#define MAX_SPHERES 4
+#define MAX_TRIANGLES 2000
+#define MAX_MESHES 4
+
+#define EPSILON 1e-5
+
+#endif
