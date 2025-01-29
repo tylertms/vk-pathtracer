6ddffbc36af71eed7ac26a882ca2c4b5478cca2d
Tyler Smith <tylertms@icloud.com>
2025-01-29 16:41:00 -0500

    Remove old print statements

--- src/Vulkan/Device.cpp
+++ src/Vulkan/Device.cpp
@@ -165,14 +165,12 @@ void Device::logInfo(const VkPhysicalDeviceProperties &properties, int index) {
 int Device::getScore(const VkPhysicalDevice &physicalDevice, VkPhysicalDeviceProperties &properties) {
     QueueFamilyIndices indices = findQueueFamilies(physicalDevice, ext_Surface);
     if (!indices.isComplete() || !deviceSupportsExtensions(physicalDevice)) {
-        printf("NO INDICES\n");
         return -1;
     }
 
     SwapChain::SupportDetails swapChainSupport = SwapChain::querySupport(physicalDevice, ext_Surface);
     bool validSwapChain = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
     if (!validSwapChain) {
-        printf("NO VALID SWAPCHAIN\n");
         return -1;
     }
 
@@ -193,7 +191,6 @@ int Device::getScore(const VkPhysicalDevice &physicalDevice, VkPhysicalDevicePro
             return i;
         }
     }
-    printf("COULDN'T FIND DEVICE TYPE\n");
 
     return -1;
 }
