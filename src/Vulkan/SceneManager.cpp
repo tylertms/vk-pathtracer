6d68f01b878c6a55c9e95c9e0b462328eb5b2631
Tyler Smith <tylertms@icloud.com>
2025-01-28 20:45:58 -0500

    Add SceneManager class to hold SSBOs

--- src/Vulkan/SceneManager.cpp
+++ src/Vulkan/SceneManager.cpp
@@ -1,22 +1,29 @@
 #include "SceneManager.h"
+#include "vulkan/vulkan_core.h"
 #include <vector>
 
 namespace Vulkan {
 
-/* ----------- FRAMES ----------- */
-void SceneManager::setFramesRendered(uint32_t framesRendered) {
-    m_Instance.framesRendered = framesRendered;
+/* ----------- INIT ----------- */
+void SceneManager::init(const Device &device) {
+    sceneUniform.init(device);
+
+    createBuffer(device, sizeof(VKPT::StorageBuffer), VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_Buffer, m_BufferMemory);
+    vkMapMemory(device.getVkDevice(), m_BufferMemory, 0, sizeof(VKPT::StorageBuffer), 0, &m_BufferMapped);
 }
 
-void SceneManager::incrementFramesRendered() {
-    m_Instance.framesRendered++;
+void SceneManager::deinit(const VkDevice &device) {
+    sceneUniform.deinit(device);
+
+    vkDestroyBuffer(device, m_Buffer, nullptr);
+    vkFreeMemory(device, m_BufferMemory, nullptr);
 }
 /* ----------------------------- */
 
 /* ----------- RESET ----------- */
 void SceneManager::resetAccumulation() {
     m_Reset = true;
-    m_Instance.framesRendered = 0;
+    sceneUniform.setFramesRendered(0);
 }
 
 bool SceneManager::resetOccurred() {
@@ -26,119 +33,30 @@ bool SceneManager::resetOccurred() {
 }
 /* ----------------------------- */
 
-/* ----------- CAMERA ----------- */
-void SceneManager::setCam(const VKPT::Camera &cam) {
-    m_Instance.camera = cam;
-    resetAccumulation();
-}
-void SceneManager::setCamAspectRatio(float aspectRatio) {
-    m_Instance.camera.aspectRatio = aspectRatio;
-    resetAccumulation();
-}
-/* ----------------------------- */
-
-/* ----------- SCENE ----------- */
-void SceneManager::loadFromFile(const std::string filename) {
-    if (filename.empty()) return;
-
-    triangleBuffer.clear();
-    modelPaths.clear();
-    m_Instance.numTriangles = 0;
-    m_Instance.numMeshes = 0;
-    m_Instance.numSpheres = 0;
-
-    Loader::loadSceneFromYAML(filename, modelPaths, m_Instance, triangleBuffer);
-
-    for (uint32_t i = 0; i < m_Instance.numMeshes; i++) {
-        applyMeshProperties(m_Instance.meshes[i]);
-    }
-
-    resetAccumulation();
-}
-
-void SceneManager::saveToFile(const std::string filename) {
-    if (filename.empty()) return;
-    Loader::saveSceneToYAML(filename, m_Instance, modelPaths);
-}
-/* ----------------------------- */
 
 /* ----------- SPHERE ----------- */
 void SceneManager::addSphere() {
-    if (m_Instance.numSpheres == MAX_SPHERES) return;
-    m_Instance.spheres[m_Instance.numSpheres++] = (VKPT::Sphere){};
-    resetAccumulation();
+    sceneUniform.data.numSpheres++;
 }
 
-void SceneManager::setNumSpheres(int numSpheres) {
-    m_Instance.numSpheres = numSpheres;
-}
 /* ----------------------------- */
 
 /* ----------- MESH ----------- */
 void SceneManager::addMesh(const std::string filename) {
     if (filename.empty()) return;
-    if (m_Instance.numMeshes == MAX_MESHES) return;
-
     Loader::GLTFLoader loader(filename);
-    std::vector<VKPT::Triangle> triangles = loader.getTriangles();
 
-    uint32_t start = m_Instance.numTriangles;
-    uint32_t count = (uint32_t)triangles.size();
-    if (start + count > MAX_TRIANGLES) return;
-
-    VKPT::Mesh mesh;
-    mesh.triangleCount = count;
-    mesh.startIndex = start;
-
-    triangleBuffer.reserve(start + count);
-    for (uint32_t i = 0; i < count; i++) {
-        m_Instance.triangles[start + i] = triangles[i];
-        triangleBuffer.push_back(triangles[i]);
+    for (auto &triangle : loader.getTriangles()) {
+        sceneStorage.triangles[sceneUniform.data.numTriangles++] = triangle;
     }
 
-    m_Instance.meshes[m_Instance.numMeshes++] = mesh;
-    m_Instance.numTriangles += count;
+    for (auto &mesh : loader.getMeshes()) {
+        sceneStorage.meshes[sceneUniform.data.numMeshes++] = mesh;
+    }
 
-    applyMeshProperties(mesh);
     modelPaths.push_back(filename);
-
-    resetAccumulation();
 }
 
-void SceneManager::applyMeshProperties(VKPT::Mesh &mesh) {
-    auto scale = mesh.scale;
-    auto rotation = mesh.rotation;
-    auto translation = mesh.translation;
-
-    glm::mat4 transformation = VKPT::createTransformationMatrix(scale, rotation, translation);
-
-    glm::vec3 boundsMin = { FLT_MAX, FLT_MAX, FLT_MAX };
-    glm::vec3 boundsMax = { -FLT_MAX, -FLT_MAX, -FLT_MAX };
-
-    for (uint32_t i = mesh.startIndex; i < mesh.startIndex + mesh.triangleCount; i++) {
-        glm::vec3 _posA = VKPT::applyTransformation(triangleBuffer[i].posA, transformation);
-        glm::vec3 _posB = VKPT::applyTransformation(triangleBuffer[i].posB, transformation);
-        glm::vec3 _posC = VKPT::applyTransformation(triangleBuffer[i].posC, transformation);
-        glm::vec3 _normA = VKPT::applyTransformation(triangleBuffer[i].normA, transformation);
-        glm::vec3 _normB = VKPT::applyTransformation(triangleBuffer[i].normB, transformation);
-        glm::vec3 _normC = VKPT::applyTransformation(triangleBuffer[i].normC, transformation);
-
-        boundsMin = glm::min(boundsMin, glm::min(_posA, glm::min(_posB, _posC)));
-        boundsMax = glm::max(boundsMax, glm::max(_posA, glm::max(_posB, _posC)));
-
-        m_Instance.triangles[i].posA = _posA;
-        m_Instance.triangles[i].posB = _posB;
-        m_Instance.triangles[i].posC = _posC;
-        m_Instance.triangles[i].normA = _normA;
-        m_Instance.triangles[i].normB = _normB;
-        m_Instance.triangles[i].normC = _normC;
-    }
-
-    mesh.bounds.min = boundsMin;
-    mesh.bounds.max = boundsMax;
-
-    resetAccumulation();
-}
 /* ----------------------------- */
 
 } // namespace Vulkan
--- src/Vulkan/SceneManager.h
+++ src/Vulkan/SceneManager.h
@@ -5,42 +5,41 @@
 #include <stdint.h>
 #include <vector>
 
-#include "../Core/Types/SceneObject.h"
-#include "../Core/Utils/Matrix.h"
+#include "SceneUniform.h"
 
-#include "../Core/Loader/SceneLoader.h"
+#include "../Core/Types/Scene.h"
+#include "../Core/Utils/Matrix.h"
 #include "../Core/Loader/GLTFLoader.h"
 
 namespace Vulkan {
 
 class SceneManager {
   public:
-    void setFramesRendered(uint32_t framesRendered);
-    void incrementFramesRendered();
+
+    void init(const Device &device);
+    void deinit(const VkDevice &device);
 
     void resetAccumulation();
     bool resetOccurred();
 
-    void setCam(const VKPT::Camera &cam);
-    void setCamAspectRatio(float aspectRatio);
-
-    void loadFromFile(const std::string filename);
-    void saveToFile(const std::string filename);
-
     void addSphere();
-    void setNumSpheres(int numSpheres);
-
     void addMesh(const std::string filename);
-    void applyMeshProperties(VKPT::Mesh &mesh);
 
-    inline VKPT::SceneObject *getObject() { return &m_Instance; }
+    inline const VkBuffer &getVkBuffer() const { return m_Buffer; }
 
+    void submitUpdates() {
+        memcpy(m_BufferMapped, &sceneStorage, sizeof(sceneStorage));
+    }
+
+    SceneUniform sceneUniform;
+    VKPT::StorageBuffer sceneStorage;
     std::vector<std::string> modelPaths;
-    std::vector<VKPT::Triangle> triangleBuffer;
 
   private:
-    VKPT::SceneObject m_Instance;
-    bool m_Reset = true;
+    bool m_Reset = false;
+    VkBuffer m_Buffer;
+    VkDeviceMemory m_BufferMemory;
+    void *m_BufferMapped;
 };
 
 } // namespace Vulkan
