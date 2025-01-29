97bc105a8e6e60ffffa5292fd5e68e373dd634e6
Tyler Smith <tylertms@icloud.com>
2025-01-29 16:42:23 -0500

    Fix local-space direction vector scaling

--- src/Shaders/Core/Scene.glsl
+++ src/Shaders/Core/Scene.glsl
@@ -19,18 +19,18 @@ HitPayload rayHitScene(Ray worldRay) {
     for (int i = 0; i < scene.numMeshes; i++) {
         Ray localRay;
         localRay.origin = vec3(meshes[i].worldLocalTransform * vec4(worldRay.origin, 1.f));
-        localRay.dir = normalize(vec3(meshes[i].worldLocalTransform * vec4(worldRay.dir, 0.f)));
-        localRay.inv = 1 / (localRay.dir + 0.0000001);
+        localRay.dir = vec3(meshes[i].worldLocalTransform * vec4(worldRay.dir, 0.f));
+        localRay.inv = 1 / localRay.dir;
 
-        float hitAABB = rayHitAABB(localRay, meshes[i].bounds);
-        if (hitAABB >= hit.distance) continue;
+        //float hitAABB = rayHitAABB(localRay, meshes[i].bounds);
+        //if (hitAABB >= hit.distance) continue;
 
         for (int t = 0; t < meshes[i].triangleCount; t++) {
             uint index = meshes[i].startIndex + t;
             temp = rayHitTriangle(localRay, triangles[index]);
             if (temp.didHit && temp.distance < hit.distance)  {
                 hit = temp;
-                hit.point = worldRay.origin + worldRay.dir * hit.distance;
+                hit.point = worldRay.origin + worldRay.dir * temp.distance;
                 hit.normal = normalize(vec3(meshes[i].localWorldTransform * vec4(temp.normal, 0.f)));
                 hit.material = meshes[i].material;
             }
