941c7ddef9088a9badde665b6f6621b15b444161
Tyler Smith <tylertms@icloud.com>
2025-01-29 20:35:15 -0500

    Add specular information

--- src/Core/Types/Material.h
+++ src/Core/Types/Material.h
@@ -15,6 +15,8 @@ struct alignas(16) Material {
     float emissionStrength = 0.0;
     vec3 emissionColor = { 1.0, 1.0, 1.0 };
     float roughness = 1.0;
+    vec3 specularColor = { 1.0, 1.0, 1.0 };
+    float specularFactor = 0.0;
 };
 }
 
@@ -26,6 +28,8 @@ struct convert<VKPT::Material> {
         node["Color"] = rhs.color;
         node["EmissionColor"] = rhs.emissionColor;
         node["EmissionStrength"] = rhs.emissionStrength;
+        node["SpecularColor"] = rhs.specularColor;
+        node["SpecularFactor"] = rhs.specularFactor;
         node["Roughness"] = rhs.roughness;
         return node;
     }
@@ -45,6 +49,16 @@ struct convert<VKPT::Material> {
             rhs.emissionColor[0] = node["EmissionColor"][0].as<float>();
             rhs.emissionColor[1] = node["EmissionColor"][1].as<float>();
             rhs.emissionColor[2] = node["EmissionColor"][2].as<float>();
+        }
+
+        if (node["SpecularColor"]) {
+            rhs.specularColor[0] = node["SpecularColor"][0].as<float>();
+            rhs.specularColor[1] = node["SpecularColor"][1].as<float>();
+            rhs.specularColor[2] = node["SpecularColor"][2].as<float>();
+        }
+
+        if (node["SpecularFactor"]) {
+            rhs.specularFactor = node["SpecularFactor"].as<float>();
         }
 
         if (node["EmissionStrength"]) {
@@ -69,6 +83,8 @@ struct Material {
     float emissionStrength;
     vec3 emissionColor;
     float roughness;
+    vec3 specularColor;
+    float specularFactor;
 };
 
 /* ---------- GLSL ---------- */
