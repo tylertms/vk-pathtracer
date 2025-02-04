3975660a0a3b864d21ba329766df6ff6911dc6e7
Tyler Smith <tylertms@icloud.com>
2025-02-04 16:49:41 -0500

    Add tone mapping and environment texture

--- src/Shaders/Core/Ray.glsl
+++ src/Shaders/Core/Ray.glsl
@@ -6,16 +6,38 @@
 
 // #define DEBUG_NORMAL
 
-Ray generateRay(vec2 uv, Camera camera, inout uint state) {
-    uv.x *= float(camera.windowSize.x) / camera.windowSize.y;
-    vec2 planeJitter = randUnitCircle(state) * camera.diverge / camera.windowSize.x;
-    vec2 originJitter = randUnitCircle(state) * camera.defocus / camera.windowSize.x;
+vec3 getEnvironmentLight(vec3 dir, uint bounceNum, inout uint state) {
+    float angle = radians(scene.camera.envRotation);
+    float cosA = cos(angle);
+    float sinA = sin(angle);
+
+    vec3 rotatedDir = vec3(
+        cosA * dir.x - sinA * dir.z,
+        dir.y,
+        sinA * dir.x + cosA * dir.z
+    );
+
+    float u = 0.5 + atan(rotatedDir.z, rotatedDir.x) * INV_2PI;
+    float v = -0.5 + asin(rotatedDir.y) * INV_PI;
+    vec2 uv = vec2(u, v);
+
+    if (bounceNum == 0) {
+        uv += randUnitCircle(state) * ENV_TEX_BLUR;
+    }
+
+    return clamp(texture(envSampler, uv).rgb, 0, 10);
+}
+
+Ray generateRay(vec2 uv, inout uint state) {
+    uv.x *= float(scene.camera.windowSize.x) / scene.camera.windowSize.y;
+    vec2 planeJitter = randUnitCircle(state) * scene.camera.diverge / scene.camera.windowSize.x;
+    vec2 originJitter = randUnitCircle(state) * scene.camera.defocus / scene.camera.windowSize.x;
 
     Ray ray;
     ray.origin = vec3(0, 0, -6);
     ray.origin += vec3(originJitter.x, originJitter.y, 0);
 
-    vec3 targetPoint = vec3((uv + planeJitter) * camera.focalDistance / 4., ray.origin.z + camera.focalDistance);
+    vec3 targetPoint = vec3((uv + planeJitter) * scene.camera.focalDistance / 4., ray.origin.z + scene.camera.focalDistance);
     ray.dir = normalize(targetPoint - ray.origin);
     ray.inv = 1.0 / ray.dir;
 
@@ -28,8 +50,10 @@ vec3 traceRay(Ray ray, uint maxBounces, inout uint state, inout uint stats[2]) {
 
     for (uint i = 0; i <= maxBounces; i++) {
         HitPayload hit = rayHitScene(ray, stats);
-        if (!hit.didHit)
+        if (!hit.didHit) {
+            incomingLight += getEnvironmentLight(ray.dir, i, state) * rayColor;
             break;
+        }
 
 #ifdef DEBUG_NORMAL
         return hit.normal * vec3(1, 1, -1);// * 0.5f + vec3(0.5);
--- src/Shaders/Graphics.frag
+++ src/Shaders/Graphics.frag
@@ -22,8 +22,9 @@ layout (binding = 0, std140) readonly uniform SceneUniform {
 scene;
 
 layout (binding = 1, rgba32f) coherent uniform image2D accumulationImage;
+layout (binding = 2) uniform sampler2D envSampler;
 
-layout (binding = 2, std430) readonly buffer SceneStorage {
+layout (binding = 3, std430) readonly buffer SceneStorage {
     BVH bvhs[MAX_BVHS];
     Triangle triangles[MAX_TRIANGLES];
 };
@@ -45,30 +46,34 @@ void main() {
 
     vec3 totalLight = vec3(0);
     for (uint i = 0; i < scene.camera.samplesPerPixel; i++) {
-        Ray ray = generateRay(fragUV, scene.camera, state);
+        Ray ray = generateRay(fragUV, state);
         totalLight += traceRay(ray, scene.camera.maxBounces, state, stats);
     }
     totalLight /= scene.camera.samplesPerPixel;
 
-    vec4 newColor = vec4(totalLight, 1);
-
 #ifdef DEBUG_BOX_TESTS
     float boxTests = stats[0] / 500.0;
     vec3 boxTestColor = boxTests > 1 ? vec3(1, 0, 0) : vec3(boxTests);
-    newColor = vec4(boxTestColor, 1);
+    outColor = vec4(boxTestColor, 1.f);
+    return;
 #endif
 
 #ifdef DEBUG_TRI_TESTS
     float triTests = stats[1] / 50.0;
     vec3 triTestColor = triTests > 1 ? vec3(1, 0, 0) : vec3(triTests);
-    newColor = vec4(triTestColor, 1);
+    outColor = vec4(triTestColor, 1.f);
+    return;
 #endif
 
     beginInvocationInterlockARB();
-    vec4 prevColor = imageLoad(accumulationImage, texCoord);
-    newColor = mix(prevColor, newColor, 1.0 / (scene.framesRendered + 1));
-    imageStore(accumulationImage, texCoord, newColor);
+    vec3 prevColor = imageLoad(accumulationImage, texCoord).rgb;
+    vec3 newColor = mix(prevColor, totalLight, 1.0 / (scene.framesRendered + 1));
+    imageStore(accumulationImage, texCoord, vec4(newColor, 1.f));
     endInvocationInterlockARB();
 
-    outColor = sqrt(newColor);
+    vec3 hdrColor = newColor * scene.camera.exposure;
+    vec3 toneMappedColor = toneMapACES(hdrColor);
+    vec3 gammaCorrectedColor = pow(toneMappedColor, vec3(1.0 / 2.2));
+
+    outColor = vec4(gammaCorrectedColor, 1.0);
 }
