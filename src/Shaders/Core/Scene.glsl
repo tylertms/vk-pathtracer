ea658edc8129bae2634dc104834d87511a704221
Tyler Smith <tylertms@icloud.com>
2025-02-06 15:43:12 -0500

    Remove old mesh intersection algorithm

--- src/Shaders/Core/Scene.glsl
+++ src/Shaders/Core/Scene.glsl
@@ -15,29 +15,6 @@ HitPayload rayHitScene(Ray worldRay, inout uint stats[2]) {
         }
     }
 
-    /*
-        for (int i = 0; i < scene.numMeshes; i++) {
-            Ray localRay;
-            localRay.origin = vec3(scene.meshes[i].worldLocalTransform * vec4(worldRay.origin, 1.f));
-            localRay.dir = vec3(scene.meshes[i].worldLocalTransform * vec4(worldRay.dir, 0.f));
-            localRay.inv = 1 / localRay.dir;
-
-            float hitAABB = rayHitAABB(localRay, scene.meshes[i].bounds);
-            if (hitAABB >= hit.distance) continue;
-
-            for (int t = 0; t < scene.meshes[i].triangleCount; t++) {
-                uint index = scene.meshes[i].startIndex + t;
-                temp = rayHitTriangle(localRay, triangles[index]);
-                if (temp.didHit && temp.distance < hit.distance)  {
-                    hit = temp;
-                    hit.point = worldRay.origin + worldRay.dir * temp.distance;
-                    hit.normal = normalize(vec3(scene.meshes[i].localWorldTransform * vec4(temp.normal, 0.f)));
-                    hit.material = scene.meshes[i].material;
-                }
-            }
-        }
-        */
-
     for (int i = 0; i < scene.numMeshes; i++) {
         Mesh mesh = scene.meshes[i];
 
