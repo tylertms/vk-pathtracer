7b5f59038f398161b238ae9b9c154c1d7fec6720
Tyler Smith <tylertms@icloud.com>
2025-02-08 19:49:56 -0500

    Add more material properties

--- src/Core/Types/Material.h
+++ src/Core/Types/Material.h
@@ -1,4 +1,3 @@
-
 #ifndef CORE_OBJECTS_MATERIAL_H
 #define CORE_OBJECTS_MATERIAL_H
 
@@ -10,14 +9,25 @@
 using namespace glm;
 
 namespace VKPT {
+
 struct alignas(16) Material {
-    vec3 color = {1.0, 1.0, 1.0};
-    float emissionStrength = 0.0;
-    vec3 emissionColor = {1.0, 1.0, 1.0};
-    float roughness = 1.0;
-    vec3 specularColor = {1.0, 1.0, 1.0};
-    float specularFactor = 0.0;
+    vec3  baseColor         = { 1.0f, 1.0f, 1.0f };
+    float alpha             = 1.0f;
+    vec3  specularTint      = { 1.0f, 1.0f, 1.0f };
+    float specular          = 0.5f;
+    vec3  emissionColor     = { 1.0f, 1.0f, 1.0f };
+    float emissionStrength  = 0.0f;
+    vec3  coatTint          = { 1.0f, 1.0f, 1.0f };
+    float coatWeight        = 0.0f;
+    vec2  IOR               = { 1.5f, 0.5f };
+    float roughness         = 0.5f;
+    float metallic          = 0.0f;
+    float transmission      = 0.0f;
+    float transmissionRoughness = 0.0f;
+    float coatRoughness     = 0.0f;
+    float coatIOR           = 1.5f;
 };
+
 } // namespace VKPT
 
 namespace YAML {
@@ -25,25 +35,45 @@ template <>
 struct convert<VKPT::Material> {
     static Node encode(const VKPT::Material &rhs) {
         Node node;
-        node["Color"] = rhs.color;
-        node["EmissionColor"] = rhs.emissionColor;
-        node["EmissionStrength"] = rhs.emissionStrength;
-        node["SpecularColor"] = rhs.specularColor;
-        node["SpecularFactor"] = rhs.specularFactor;
-        node["Roughness"] = rhs.roughness;
+        node["BaseColor"]             = rhs.baseColor;
+        node["Alpha"]                 = rhs.alpha;
+        node["SpecularTint"]          = rhs.specularTint;
+        node["Specular"]              = rhs.specular;
+        node["EmissionColor"]         = rhs.emissionColor;
+        node["EmissionStrength"]      = rhs.emissionStrength;
+        node["CoatTint"]              = rhs.coatTint;
+        node["CoatWeight"]            = rhs.coatWeight;
+        node["IOR"]                   = rhs.IOR;
+        node["Roughness"]             = rhs.roughness;
+        node["Metallic"]              = rhs.metallic;
+        node["Transmission"]          = rhs.transmission;
+        node["TransmissionRoughness"] = rhs.transmissionRoughness;
+        node["CoatRoughness"]         = rhs.coatRoughness;
+        node["CoatIOR"]               = rhs.coatIOR;
         return node;
     }
 
     static bool decode(const Node &node, VKPT::Material &rhs) {
-        if (!node.IsMap()) {
+        if (!node.IsMap())
             return false;
+
+        if (node["BaseColor"]) {
+            rhs.baseColor[0] = node["BaseColor"][0].as<float>();
+            rhs.baseColor[1] = node["BaseColor"][1].as<float>();
+            rhs.baseColor[2] = node["BaseColor"][2].as<float>();
         }
 
-        if (node["Color"]) {
-            rhs.color[0] = node["Color"][0].as<float>();
-            rhs.color[1] = node["Color"][1].as<float>();
-            rhs.color[2] = node["Color"][2].as<float>();
+        if (node["Alpha"])
+            rhs.alpha = node["Alpha"].as<float>();
+
+        if (node["SpecularTint"]) {
+            rhs.specularTint[0] = node["SpecularTint"][0].as<float>();
+            rhs.specularTint[1] = node["SpecularTint"][1].as<float>();
+            rhs.specularTint[2] = node["SpecularTint"][2].as<float>();
         }
+
+        if (node["Specular"])
+            rhs.specular = node["Specular"].as<float>();
 
         if (node["EmissionColor"]) {
             rhs.emissionColor[0] = node["EmissionColor"][0].as<float>();
@@ -51,23 +81,40 @@ struct convert<VKPT::Material> {
             rhs.emissionColor[2] = node["EmissionColor"][2].as<float>();
         }
 
-        if (node["SpecularColor"]) {
-            rhs.specularColor[0] = node["SpecularColor"][0].as<float>();
-            rhs.specularColor[1] = node["SpecularColor"][1].as<float>();
-            rhs.specularColor[2] = node["SpecularColor"][2].as<float>();
-        }
-
-        if (node["SpecularFactor"]) {
-            rhs.specularFactor = node["SpecularFactor"].as<float>();
-        }
-
-        if (node["EmissionStrength"]) {
+        if (node["EmissionStrength"])
             rhs.emissionStrength = node["EmissionStrength"].as<float>();
+
+        if (node["CoatTint"]) {
+            rhs.coatTint[0] = node["CoatTint"][0].as<float>();
+            rhs.coatTint[1] = node["CoatTint"][1].as<float>();
+            rhs.coatTint[2] = node["CoatTint"][2].as<float>();
         }
 
-        if (node["Roughness"]) {
-            rhs.roughness = node["Roughness"].as<float>();
+        if (node["CoatWeight"])
+            rhs.coatWeight = node["CoatWeight"].as<float>();
+
+        if (node["IOR"]) {
+            rhs.IOR[0] = node["IOR"][0].as<float>();
+            rhs.IOR[1] = node["IOR"][1].as<float>();
         }
+
+        if (node["Roughness"])
+            rhs.roughness = node["Roughness"].as<float>();
+
+        if (node["Metallic"])
+            rhs.metallic = node["Metallic"].as<float>();
+
+        if (node["Transmission"])
+            rhs.transmission = node["Transmission"].as<float>();
+
+        if (node["TransmissionRoughness"])
+            rhs.transmissionRoughness = node["TransmissionRoughness"].as<float>();
+
+        if (node["CoatRoughness"])
+            rhs.coatRoughness = node["CoatRoughness"].as<float>();
+
+        if (node["CoatIOR"])
+            rhs.coatIOR = node["CoatIOR"].as<float>();
 
         return true;
     }
@@ -79,12 +126,21 @@ struct convert<VKPT::Material> {
 /* ---------- GLSL ---------- */
 
 struct Material {
-    vec3 color;
-    float emissionStrength;
+    vec3 baseColor;
+    float alpha;
+    vec3 specularTint;
+    float specular;
     vec3 emissionColor;
+    float emissionStrength;
+    vec3 coatTint;
+    float coatWeight;
+    vec2 IOR;
     float roughness;
-    vec3 specularColor;
-    float specularFactor;
+    float metallic;
+    float transmission;
+    float transmissionRoughness;
+    float coatRoughness;
+    float coatIOR;
 };
 
 /* ---------- GLSL ---------- */
