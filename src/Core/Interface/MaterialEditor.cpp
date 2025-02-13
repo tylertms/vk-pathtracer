f2df828a2907db091f4ca119c4890beb52f85be3
Tyler Smith <tylertms@icloud.com>
2025-02-13 09:51:56 -0500

    Update GUI to use new material properties

--- src/Core/Interface/MaterialEditor.cpp
+++ src/Core/Interface/MaterialEditor.cpp
@@ -31,13 +31,15 @@ void drawMaterialEditor(Vulkan::SceneManager &sceneManager) {
         VKPT::Material &material = sceneManager.sceneData.materials[i];
         bool changed = false;
 
-        const char *materialHeader = (std::string("Material ") + std::to_string(i)).c_str();
 
-        if (ImGui::CollapsingHeader(materialHeader, i == objectMatIndex ? ImGuiTreeNodeFlags_DefaultOpen : 0)) {
+        if (ImGui::CollapsingHeader((std::string("Material ") + std::to_string(i)).c_str(), 
+                i == objectMatIndex ? ImGuiTreeNodeFlags_DefaultOpen : 0))
+            {
+                
             if (ImGui::ColorEdit3("Base Color", glm::value_ptr(material.baseColor))) changed = true;
             if (ImGui::SliderFloat("Roughness", &material.roughness, 0.0f, 1.0f)) changed = true;
             if (ImGui::DragFloat2("IOR", glm::value_ptr(material.IOR), 0.01f, 0.f, 100.f)) changed = true;
-            if (ImGui::DragFloat("Specular", &material.specular, 0.01f, 0.0f, 12.5f)) changed = true;
+            if (ImGui::SliderFloat("Metallic", &material.metallic, 0.0f, 1.0f)) changed = true;
             if (ImGui::ColorEdit3("Specular Tint", glm::value_ptr(material.specularTint))) changed = true;
             if (ImGui::DragFloat("Emission Strength", &material.emissionStrength, 0.01f, 0.0f, 1000.f)) changed = true;
             if (ImGui::ColorEdit3("Emission Color", glm::value_ptr(material.emissionColor))) changed = true;
