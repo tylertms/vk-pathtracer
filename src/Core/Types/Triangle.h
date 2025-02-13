67a65fb47e49958c825a744e01998c79e37dc228
Tyler Smith <tylertms@icloud.com>
2025-02-13 09:50:21 -0500

    Add tangent and uv data to triangles

--- src/Core/Types/Triangle.h
+++ src/Core/Types/Triangle.h
@@ -8,12 +8,10 @@ using namespace glm;
 
 namespace VKPT {
     struct alignas(16) Triangle {
-        alignas(16) vec3 posA;
-        alignas(16) vec3 posB;
-        alignas(16) vec3 posC;
-        alignas(16) vec3 normA;
-        alignas(16) vec3 normB;
-        alignas(16) vec3 normC;
+        vec4 pos[3];
+        vec4 norm[3];
+        vec4 tan[3];
+        vec2 uv0[3];
     };
 }
 
@@ -22,42 +20,51 @@ namespace VKPT {
 /* ---------- GLSL ---------- */
 
 struct Triangle {
-    vec3 posA;
-    vec3 posB;
-    vec3 posC;
-    vec3 normA;
-    vec3 normB;
-    vec3 normC;
+    vec3 pos[3];
+    vec3 norm[3];
+    vec3 tan[3];
+    vec2 uv0[3];
 };
 
 HitPayload rayHitTriangle(Ray ray, Triangle tri) {
-    vec3 eAB = tri.posB - tri.posA;
-    vec3 eAC = tri.posC - tri.posA;
+    vec3 eAB = tri.pos[1] - tri.pos[0];
+    vec3 eAC = tri.pos[2] - tri.pos[0];
     vec3 nG  = cross(eAB, eAC);
+    
     float d  = -dot(ray.dir, nG);
     float ad = abs(d);
     float passDet = step(DST_EPSILON, ad);
     float invD = 1.0f / (d + 1e-30f);
-    vec3 ao  = ray.origin - tri.posA;
+
+    vec3 ao  = ray.origin - tri.pos[0];
     vec3 dao = cross(ao, ray.dir);
+
     float t  = dot(ao, nG) * invD;
     float u  = dot(eAC, dao) * invD;
     float v  = -dot(eAB, dao) * invD;
     float w  = 1.0f - u - v;
+
     float passT = step(DST_EPSILON, t);
     float passU = step(0.0f, u);
     float passV = step(0.0f, v);
     float passW = step(0.0f, w);
     float passAll = passDet * passT * passU * passV * passW;
 
-    vec3 nI = normalize(tri.normA * w + tri.normB * u + tri.normC * v);
+    vec3 nI = normalize(tri.norm[0] * w + tri.norm[1] * u + tri.norm[2] * v);
+    vec2 hitUV0 = tri.uv0[0] * w + tri.uv0[1] * u + tri.uv0[2] * v;
+    vec3 nT = normalize(tri.tan[0] * w + tri.tan[1] * u + tri.tan[2] * v);
 
     HitPayload p;
-    p.didHit   = (passAll != 0.0f);
+    p.didHit = (passAll != 0.0f);
     p.distance = mix(0.0f, t, passAll);
-    vec3 hitP  = ray.origin + ray.dir * t + nI * 1e6;
-    p.point    = mix(vec3(0.0f), hitP, passAll);
-    p.normal   = mix(vec3(0.0f), nI,   passAll);
+
+    vec3 hitP = ray.origin + ray.dir * t + nI * 1e6;
+
+    p.point = mix(vec3(0.0f), hitP, passAll);
+    p.normal = mix(vec3(0.0f), nI, passAll);
+    p.tangent = mix(vec3(0.0f), nT, passAll);
+    p.uv0 = mix(vec2(0.0f), hitUV0, passAll);
+
     return p;
 }
 
